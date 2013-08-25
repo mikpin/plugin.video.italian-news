62410317616df5298ba9a054c87de889bf8bdedf
mikpin <michelepinto77@gmail.com>
2013-08-25 22:09:28 +0200

    added icon.png

--- default.py
+++ default.py
@@ -10,7 +10,19 @@ def _addItem(label,uri):
     item = xbmcgui.ListItem(label)
     xbmcplugin.addDirectoryItem(_thisPlugin,uri,item)
 
-_addItem('paolo','')
-_addItem('luca','')
-
-xbmcplugin.endOfDirectory(_thisPlugin)
+def _get_params():
+    param=[] _addItem('paolo','') _addItem('luca','')
+    paramstring=sys.argv[2] xbmcplugin.endOfDirectory(_thisPlugin)
+    if len(paramstring)>=2:
+            params=sys.argv[2]
+            cleanedparams=params.replace('?','')
+            if (params[len(params)-1]=='/'):
+                    params=params[0:len(params)-2]
+            pairsofparams=cleanedparams.split('&')
+            param={}
+            for i in range(len(pairsofparams)):
+                    splitparams={}
+                    splitparams=pairsofparams[i].split('=')
+                    if (len(splitparams))==2:
+                            param[splitparams[0]]=splitparams[1]
+    return param
