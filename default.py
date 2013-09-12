7ba65a6a1c890bffe234d6d473bafc61523511b9
mikpin <michelepinto77@gmail.com>
2013-09-12 11:30:55 +0200

    ---

--- default.py
+++ default.py
@@ -1,18 +1,23 @@
-import sys, xbmcplugin, xbmcgui
+import sys, xbmcplugin, xbmcgui,xbmc
 
 _id = "plugin.video.italian-news"
-_resdir = "special://home/addons" + _id + "/resources"
+_resdir = "special://home/addons/" + _id + "/resources"
 _thisPlugin = int(sys.argv[1])
+_icons = _resdir + "/icons/"
 
-sys.path.append( _resdir + "/lib/")
+sys.path.append( xbmc.translatePath(_resdir + "/lib/"))
+import rai
+
+tg1icona=xbmc.translatePath(_icons +"tg1.jpg")
+
 
 def _addItem(label,uri):
-    item = xbmcgui.ListItem(label)
+    item = xbmcgui.ListItem(label, iconImage=tg1icona)
     xbmcplugin.addDirectoryItem(_thisPlugin,uri,item)
 
 def _get_params():
-    param=[] _addItem('paolo','') _addItem('luca','')
-    paramstring=sys.argv[2] xbmcplugin.endOfDirectory(_thisPlugin)
+    param=[]
+    paramstring=sys.argv[2]
     if len(paramstring)>=2:
             params=sys.argv[2]
             cleanedparams=params.replace('?','')
@@ -26,3 +31,9 @@ def _get_params():
                     if (len(splitparams))==2:
                             param[splitparams[0]]=splitparams[1]
     return param
+
+tg1 = rai.RaiUno().get()
+print tg1
+for (name,url,icon) in tg1:
+    _addItem(name,url)
+xbmcplugin.endOfDirectory(_thisPlugin)
