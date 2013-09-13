f0de27765d801278256e5f693f833c9d40605dd9
mikxbmc <michelepinto77@gmail.com>
2013-09-13 06:47:23 +0200

    added plugin support

--- default.py
+++ default.py
@@ -11,9 +11,9 @@ import rai
 _tg1Icon=xbmc.translatePath(_icons +"tg1.jpg")
 
 
-def _addItem(label,uri,icon):
+def _addItem(label,uri,icon,isFolder=False):
     item = xbmcgui.ListItem(label, iconImage=icon)
-    xbmcplugin.addDirectoryItem(_thisPlugin,uri,item)
+    xbmcplugin.addDirectoryItem(_thisPlugin,uri,item,isFolder)
 
 def _get_params():
     param=[]
@@ -32,8 +32,22 @@ def _get_params():
                             param[splitparams[0]]=splitparams[1]
     return param
 
-tg1 = rai.RaiUno().get()
-print param
-for (name,url,icon) in tg1:
-    _addItem(name,url,icon)
-xbmcplugin.endOfDirectory(_thisPlugin)
+param = _get_params()
+plugins = {
+    '1':(rai.RaiUno, 'Guarda il TG1',_tg1Icon)
+    }
+
+if 'plugin' in param:
+    (engine, title, icon)=plugins[param['plugin']]
+    for (name,url,icon) in engine().get():
+        _addItem(name,url,icon)
+    xbmcplugin.endOfDirectory(_thisPlugin)
+else:
+    for n in plugins:
+        (engine, title, icon)=plugins[n]
+        _addItem(title,sys.argv[0]+'?plugin='+n,icon,isFolder=True)
+    xbmcplugin.endOfDirectory(_thisPlugin)
+    
+#for (name,url,icon) in tg1:
+#    _addItem(name,url,icon)
+#xbmcplugin.endOfDirectory(_thisPlugin)
