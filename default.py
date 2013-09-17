5b3c7be13e614970beefdf3be76c87ace19c24a3
mikxbmc <michelepinto77@gmail.com>
2013-09-17 13:57:14 +0200

    definitve 0.0.1

--- default.py
+++ default.py
@@ -8,8 +8,9 @@ _icons = _resdir + "/icons/"
 sys.path.append( xbmc.translatePath(_resdir + "/lib/"))
 import rai
 
-_tg1Icon=xbmc.translatePath(_icons +"tg1.jpg")
-
+_tg1Icon=xbmc.translatePath(_icons +"Tg1_logo.png")
+_tg2Icon=xbmc.translatePath(_icons +"Tg2_logo.png")
+_tg3Icon=xbmc.translatePath(_icons +"Tg3_logo.png")
 
 def _addItem(label,uri,icon,isFolder=False):
     item = xbmcgui.ListItem(label, iconImage=icon)
@@ -34,17 +35,22 @@ def _get_params():
 
 param = _get_params()
 plugins = {
-    '1':(rai.RaiUno, 'Guarda il TG1',_tg1Icon)
+    '1':(rai.RaiUno, 'Guarda il TG1',_tg1Icon),
+    '2':(rai.RaiDue, 'Guarda il TG2',_tg2Icon),
+    '3':(rai.RaiTre, 'Guarda il TG3',_tg3Icon)
     }
 
 if 'plugin' in param:
-    (engine, title, icon)=plugins[param['plugin']]
+    (engine, title, eicon)=plugins[param['plugin']]
     for (name,url,icon) in engine().get():
+        if icon == '':
+            icon = eicon
         _addItem(name,url,icon)
     xbmcplugin.endOfDirectory(_thisPlugin)
 else:
-    for n in plugins:
+    for n in sorted(plugins.iterkeys()):
         (engine, title, icon)=plugins[n]
+        print title
         _addItem(title,sys.argv[0]+'?plugin='+n,icon,isFolder=True)
     xbmcplugin.endOfDirectory(_thisPlugin)
     
--- resources/lib/rai.py
+++ resources/lib/rai.py
@@ -1,6 +1,7 @@
-import urllib2,re
-import xml.etree.ElementTree as ElementTree
+import urllib2, json
+from operator import itemgetter
 
+"""
 class RaiNews:
     def __init__(self):
         pass
@@ -8,6 +9,28 @@ class RaiNews:
         f = urllib2.urlopen(uri)
         data = f.read()
         return (f.getcode() , data)
+    def parseItemXML(self,itemXMLurl):
+        (code,itemXML) = self.load(itemXMLurl)
+        try:
+            xml = ElementTree.fromstring(itemXML)
+        except:
+            pass
+        item = xml.find('./item')
+        name = item.attrib['name']
+        videoUnitURL = xml.find('./item/units/videoUnit/url').text
+        try:
+            imageUnitURL = self.baseUrl+xml.find('./item/units/imageUnit/image').text
+        except:
+            imageUnitURL = ''
+        videoUnitAttribute =xml.find('./item/units/videoUnit/attributes').findall('./attribute')
+        
+        for node in videoUnitAttribute:
+            key = node.find('key').text
+            value = node.find('value').text
+            if (key=='h264'):
+                videoUnitURL = value
+        
+        return (name,videoUnitURL, imageUnitURL)
 
 class RaiUno(RaiNews):
     def __init__(self):
@@ -34,6 +57,7 @@ class RaiUno(RaiNews):
                 videoUnitURL = xml.find('./item/units/videoUnit/url').text
                 imageUnitURL = self.baseUrl+xml.find('./item/units/imageUnit/image').text
                 videoUnitAttribute =xml.find('./item/units/videoUnit/attributes').findall('./attribute')
+
                 for node in videoUnitAttribute:
                     key = node.find('key').text
                     value = node.find('value').text
@@ -43,6 +67,62 @@ class RaiUno(RaiNews):
             return ret
                 
 
+class RaiDue(RaiNews):
+    def __init__(self):
+        self.baseUrl="http://www.tg2.rai.it/"
+        self.reEdizione = re.compile('<a data-ediz="(.*?)"(.*?)data-uniq="(.*?)"',re.DOTALL)
+    def get(self):
+        ret = []
+        (code,html) = self.load(self.baseUrl)
+        edizioni = self.reEdizione.finditer(html)
+        for e in edizioni:
+            itemXMLurl = self.baseUrl+'/dl/RaiTV/programmi/media/'+e.group(3)+'.xml'
+            ret.append( self.parseItemXML(itemXMLurl))
+        return ret
+"""
+class RaiJson():
+    def __init__(self):
+        self.content = []
+        pass
+    def load(self,uri):
+        print "loading: " + uri
+        f = urllib2.urlopen(uri)
+        data = f.read()
+        return (f.getcode() , data)
+    def loadPage(self,page):
+        return self.load('http://www.rai.tv/dl/RaiTV/programmi/json/liste/' + self.content + '-json-V-'+str(page)+'.html')
+    def loadContent(self):
+        ret = []
+        for c in self.content:
+            (error,jsonRai) =self.load('http://www.rai.tv/dl/RaiTV/programmi/json/liste/' + c + '-json-V-0.html')
+            data= json.loads(jsonRai)
+            list = data['list']
+            ret = ret+list
+        ret=sorted(ret, key=itemgetter('name'))
+        return ret
+    def get(self,page = 0):
+        list = self.loadContent()
+        ret=[]
+        for e in list:
+            image = ''
+            if e['image_433'] != '':
+                image = 'http://www.rai.tv'+e['image_433']
+            ret.append((e['name'],e['h264'],image))
+        return ret
+
+class RaiUno(RaiJson):
+    def __init__(self):
+        self.content = ['ContentSet-fb594231-cfb2-4e5a-b121-196ca772e335']
+
+class RaiDue(RaiJson):
+    def __init__(self):
+        self.content = ['ContentSet-995870b7-67cf-4e13-ab9c-2cbabd026d7e','ContentSet-46ce6c60-c7f2-446c-8a61-fc2153fbe2d3',
+            'ContentSet-5e2e70b2-f815-46ee-9011-d5fce1569147','ContentSet-81bb6f30-9284-439a-aaa4-21a5c91652b7']
+            
+class RaiTre(RaiJson):
+    def __init__(self):
+        self.content = ["ContentSet-386b19cb-40f7-447c-8cb5-eb32ece56701","ContentSet-6734e497-4206-4669-88c5-91a7a5f00a9b","ContentSet-9e4448b6-0998-4d63-937d-27b1651c6f86"]
+            
 if __name__ =='__main__':
     rai1 = RaiUno()
     print rai1.get()
